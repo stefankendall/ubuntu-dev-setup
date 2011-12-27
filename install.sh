305fbb8082c6743312058a623e7e55844e25c4a5
skendall <skendall@skendall-dell.(none)>
2011-12-26 19:15:26 -0500

    Made everything clean and download always

--- install.sh
+++ install.sh
@@ -6,28 +6,23 @@ GROOVY="groovy-binary-1.8.5"
 
 mkdir -p tools
 
+rm -rf ./tools/*
+
 wget http://dist.groovy.codehaus.org/distributions/$GROOVY.zip
-if [ ! -e ./tools/$GROOVY ]
-then
 unzip $GROOVY.zip 
 rm $GROOVY.zip
 mv groovy* ./tools
-fi
-
 
 wget http://repo.gradle.org/gradle/distributions/$GRADLE-all.zip 
-if [ ! -e ./tools/$GRADLE ]
-then
 unzip $GRADLE-all.zip 
 rm $GRADLE-all.zip
 mv gradle* ./tools
-fi
 
 cp -R ./tools /
 
 rm /tools/gradle
-ln -s /tools/$GRADLE /tools/gradle
+ln -s /tools/gradle* /tools/gradle
 
 rm /tools/groovy
-ln -s /tools/$GROOVY /tools/groovy
-
+ln -s /tools/groovy* /tools/groovy
+source /etc/environment
