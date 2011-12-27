4bf36eb24509708509227af5a18af0b07c43f3f5
skendall <skendall@skendall-dell.(none)>
2011-12-26 19:11:03 -0500

    Fixing environment, install script

--- install.sh
+++ install.sh
@@ -4,26 +4,23 @@ cp ./etc/environment /etc/environment
 GRADLE="gradle-1.0-milestone-6"
 GROOVY="groovy-binary-1.8.5"
 
-if [ ! -e ./$GROOVY.zip ]
-then
-wget http://dist.groovy.codehaus.org/distributions/$GROOVY.zip
-fi
+mkdir -p tools
 
+wget http://dist.groovy.codehaus.org/distributions/$GROOVY.zip
 if [ ! -e ./tools/$GROOVY ]
 then
-unzip $GROOVY.zip -d $GROOVY
-mv $GROOVY/* ./tools
+unzip $GROOVY.zip 
+rm $GROOVY.zip
+mv groovy* ./tools
 fi
 
-if [ ! -e ./$GRADLE-all.zip ]
-then
+
 wget http://repo.gradle.org/gradle/distributions/$GRADLE-all.zip 
-fi
-
 if [ ! -e ./tools/$GRADLE ]
 then
-unzip $GRADLE-all.zip -d $GRADLE
-mv $GRADLE/* ./tools
+unzip $GRADLE-all.zip 
+rm $GRADLE-all.zip
+mv gradle* ./tools
 fi
 
 cp -R ./tools /
