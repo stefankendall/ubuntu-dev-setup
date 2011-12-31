2596ac1d400b5ae4343a3d4fa04ce7b366b770d3
skendall <skendall@skendall-dell.(none)>
2011-12-31 14:47:33 -0500

    Added gitignore. Added chromedrive for cucumber tests

--- rvm.sh
+++ rvm.sh
@@ -1,3 +1,7 @@
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 rvm pkg install zlib
 rvm install 1.9.3 -C --with-zlib-dir=$rvm_path/usr
+wget http://chromium.googlecode.com/files/chromedriver_linux64_17.0.963.0.zip
+unzip chromedriver*
+rm chromedriver*.zip
+sudo mv ./chromedriver /usr/bin
